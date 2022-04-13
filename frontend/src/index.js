be783331ecab09ca47efa3197946df8c79922931
annarad111 <radulescuana111@gmail.com>
2022-04-13 10:15:12 +0300

    Changed background color to dark green

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from "../src/components/Navbar/navbar.js";
-import Home from "./pages";
-import About from "./pages/about";
-import Contact from "./pages/contact";
-import Gallery from "./pages/gallery";
-import Services from "./pages/services";
-import Footer from "./components/Footer/Footer.js";
-import history from "./history.js";
 import {
   HashRouter,
   Routes,
@@ -18,8 +10,7 @@ import {
   Outlet,
   Navigate,
 } from "react-router-dom";
-import Videos from "./components/Videos/Videos.js";
-import Photos from "./components/Photos/Photos.js";
+
 
 ReactDOM.render(
   <React.StrictMode>
